7e6a929a66593b2faa246aac09def519deaf5999
utkarsh pathak <utkarsh.pathak@remotestate.com>
2025-01-24 11:56:24 +0530

    idk some changes

--- jsDSA/34.js
+++ jsDSA/34.js
@@ -1,13 +1,16 @@
-var searchRange = function(nums, target) {
-    return nums.reduce((arr, val, index) => {
-        if (target === val) {
-            if (arr[0] === -1) {
-                arr[0] = index; 
-            }
-            arr[1] = index;
+var searchRange = function (nums, target) {
+  return nums.reduce(
+    (arr, val, index) => {
+      if (target === val) {
+        if (arr[0] === -1) {
+          arr[0] = index;
         }
-        return arr;
-    },[-1,-1]);
+        arr[1] = index;
+      }
+      return arr;
+    },
+    [-1, -1]
+  );
 };
 
-console.log(searchRange([1,2,3,2,3,4,1,2,5,4],2))
+console.log(searchRange([1, 2, 3, 2, 3, 4, 1, 2, 5, 4], 2));
